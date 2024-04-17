4e1cff47986e8763b5e8713699d68948549a2f89
david <davidq82@gmail.com>
2024-04-17 20:57:04 -0300

    template

--- index.php
+++ index.php
@@ -2,7 +2,6 @@
 
 require_once "Core/Controllers/TemplateController.php";
 
-
 require_once __DIR__ . './vendor/autoload.php';  // Asegúrate de que la ruta apunte al archivo autoload.php de Composer
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/'); // Apunta al directorio raíz del proyecto
 $dotenv->load();
